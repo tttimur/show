71305778e0b53ce7a75fdb9a4d4f3e8f3efd203a
black_knight <timur@56.digital>
2017-08-24 01:00:27 -0400

    dat read file and play video

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-var p = {
-	init : function () {
+async function main () {
+  // create an archive instance for the current website
+  var archive = new DatArchive(window.location.toString())
+  var video = document.querySelector('video')
 
-	}
+  var files = await archive.readdir('/videos/')
+  video.setAttribute('src', 'videos/' + files[0])
+  video.play()
+  console.log(files)
 }
 
-p.init()
+main()
