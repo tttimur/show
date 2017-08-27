1f8976e874167d578e533e994dda932298cbaa3b
black knight <timur@56.digital>
2017-08-26 23:08:50 -0400

    get videos in the /videos route

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 
 app.use(express.static('app'))
+
+var dirname = __dirname + '/app/videos'
+let videosList = []
+
+fs.readdir(dirname, (err, files) => {
+	if (err) {
+		console.log(err)
+		return
+	}
+
+	files.forEach(file => {
+		videosList.push(file)
+	})
+})
+
+
+app.get('/videos', (req, res) => {
+	res.send(videosList)
+})
 
 app.listen(3000)
